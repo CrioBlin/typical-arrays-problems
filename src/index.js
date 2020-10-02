9764d6c573fa419afc937087399ec7955dceba31
Oleksandr Kaminetskyi <crioblin11@gmail.com>
2020-10-02 10:39:51 +0300

    feat: implement min&max functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+  if (array == 0) return 0;
+
+  array = array.sort( (a,b) => {return a - b});
+  if (array[0]) return array[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+  if (array == 0) return 0;
+  
+  array = array.sort( (a,b) => {return b - a});
+  if (array[0]) return array[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+  if (array == 0) return 0;
+  let sum = 0;
+
+  array.forEach( () => {})
+
 }
